a852656f41786e4d81b0df6377cb35533b21a42b
Clement Skau <clementskau@gmail.com>
2012-04-16 13:16:08 +0200

    Fleshes out stubs in Node a bit

--- tinfoil.py
+++ tinfoil.py
@@ -11,6 +11,12 @@ import twisted.internet.reactor
 class Node:
   def __init__(self, udpPort=4000):
     self.udpPort = udpPort
+    # TODO(cskau): we need to ask the network for last known sequence number
+    self.sequenceNumber = 0
+    self.userID = None
+    # TODO(cskau): maybe securely store these in the network so we don't 
+    #  lose them. Retrieve every time we join.
+    self.postKeys = {}
 
   ''' Join the social network.
     Calculate our userID and join network at given place.
@@ -33,6 +39,17 @@ class Node:
     self.node = EntangledNode(udpPort=self.udpPort)
     self.node.joinNetwork(knownNodes)
     twisted.internet.reactor.run()
+    # TODO(cskau): stub~~
+    if "joining for the first time":
+      self.userID = self._getRandomIDFromNetwork()
+    else:
+      self.userID = 'our previously issued ID'
+
+  ''' Ask network to generate a pseudo random ID for us, a la FreeNet
+  '''
+  def _getRandomIDFromNetwork(self):
+    # TODO(cskau): stub
+    return 0
 
   ''' Share some stored resource with one or more users.
     Allow other user(s) to access store resource by issuing sharing key
@@ -45,8 +62,24 @@ class Node:
           "SharingKey(resourceID, otherUserID)",
           sharingKeys[resourceID][otherUserID])
   '''
-  def share(self):
-    pass
+  def share(self, resourceID, friendsID):
+    sharingKeyID = ('%s:share:%s' % (resourceID, friendsID))
+    sharingKey = self._encryptForUser(self.postKeys[resourceID], friendsID)
+    self.node.publishData(sharingKeyID, sharingKey)
+
+  ''' Encrypt some content assymetrically with user's public key
+  '''
+  def _encryptForUser(self, content, userID):
+    userKey = self._getUserPublicKey(userID)
+    return self._encryptKey(content, userKey)
+
+  def _getUserPublicKey(self, userID):
+    publicKeyID = ('%s:publickey' % (userID))
+    return self.node.iterativeFindValue(publicKeyID)
+
+  def _encryptKey(self, content, userKey):
+    # TODO(cskau): stub
+    return content
 
   ''' Unshare previously shared resource with one of more users.
     Ask network to delete specific, existing sharing keys.
@@ -56,8 +89,9 @@ class Node:
     Code Sketch:
       weakDelete(sharingKeys[resourceID][otherUserID])
   '''
-  def unshare(self):
-    pass
+  def unshare(self, resourceID, friendsID):
+    shareKeyID = ('%s:share:%s' % (resourceID, friendsID))
+    self.node.removeDate(shareKeyID)
 
   ''' Post some resource to the network.
     Ask the network to store some (encrypted) resource.
@@ -67,8 +101,29 @@ class Node:
     Code Sketch:
       ...
   '''
-  def post(self):
-    pass
+  def post(self, content):
+    newSequenceNumber = self._getSequenceNumber()
+    encryptedContent = self._encryptPost(key, content)
+    # We need to store post keys used so we can issue sharing keys later
+    self.postKeys[newSequenceNumber] = key
+    postID = ('%s:post:%s' % (self.userID, newSequenceNumber))
+    self.node.publishData(postID, encryptedContent)
+    # update our latest sequence number
+    self.node.publishData('%s:latest', newSequenceNumber)
+
+  ''' Return next, unused sequence number unique to this user
+  '''
+  def _getSequenceNumber(self):
+    # TODO(cskau): we probasbly need to ask the network to avoid sync errors
+    #  a user might publish from multiple clients at a time
+    self.sequenceNumber += 1
+    return self.sequenceNumber
+
+  ''' Encrypt a post with a symmetric key
+  '''
+  def _encryptPost(self, key, post):
+    # TODO(cskau): This is a stub
+    return post
 
   ''' Check for and fetch new updates on user(s)
     Ask for latest known post from a given user and fetch delta since last
@@ -78,8 +133,14 @@ class Node:
       latestPostID = hash(otherUserID + latestSequenceNumber)
       latestPost = get(latestPostID)
   '''
-  def getUpdates(self):
-    pass
+  def getUpdates(self, friendsID, lastKnownSequenceNumber):
+    latestSequenceNumber = self.node.iterativeFindValue(
+        ('%s:latest' % (friendsID)))
+    delta = {}
+    for n in range(lastKnownSequenceNumber, latestSequenceNumber):
+      postID = ('%s:post:%s' % (friendsID, n))
+      delta[postID] = self.node.iterativeFindValue(postID)
+    return delta
 
 
 if __name__ == '__main__':
