233cb64b54e66df11d490c1a4e87fe3cdb56f4b9
Allan <allan.stisen@gmail.com>
2012-04-23 12:58:01 +0200

    Changed protocol to support s/kademlia Routing table maintenance

--- tinfoil.py
+++ tinfoil.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: UTF-8
 
-"""The Tinfoil social network client."""
+'''
+  The Tinfoil social network client.
+'''
 
 from tintangled import EntangledNode
 import twisted.internet.reactor
@@ -9,57 +11,41 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto import Random
-from Crypto.Random import random
-
-
-SYMMETRIC_KEY_LENGTH = 32 # (bytes)
-SYMMETRIC_KEY_NOUNCE = 0xbeefcafe
-RSA_BITS = 2048
-
+from tintangled_protocol import TintangledProtocol
 
 class Node:
-
-  def __init__(self, udpPort = 4000):
-    """Initializes a Tinfoil Node."""
+  def __init__(self, udpPort=4000):
     self.udpPort = udpPort
     # TODO(cskau): we need to ask the network for last known sequence number
     self.sequenceNumber = 0
     self.userID = None
-    # TODO(cskau): maybe securely store these in the network so we don't
+    # TODO(cskau): maybe securely store these in the network so we don't 
     #  lose them. Retrieve every time we join.
     self.postKeys = {}
-    self.secRandom = Random.new() # cryptographically safe Random function.
-    self.RSAkey = RSA.generate(RSA_BITS, self.secRandom.read)
+    self.random = Random.new()
+    self.RSAkey = RSA.generate(2048, self.random.read)
 
-    # NOTE(purbak): right now the public/private keys are just generated on
-    # initialization of the node rather than on join. Is this a problem?
-    # public key can be extracted using self.RSAkey.publickey()
-    # i.e. self.publickey = self.RSAkey.publickey()
-
-    # NOTE(purbak): Hard to find examples of crypto challenges online.
-    # Alternatively, hash a string of sufficient length and let a newcomer
-    # bruteforce it. (I can create a function for it if need be).
-
-  def join(self, knownNodes):
-    '''Join the social network.
+  ''' Join the social network.
     Calculate our userID and join network at given place.
     This involves:
-    - requesting a random ID from the network.
-    - generate public and private keys for new id.
-    - notifying and requesting involved parties of the selected position.
+     - requesting a random ID from the network.
+     - generate public and private keys for new id.
+     - notifying and requesting involved parties of the selected position.
     OR if the user has already created his ID in the past.
-    - use the previously established private key to authenticate in network.
+     - use the previously established private key to authenticate in network.
     Note:
-    The protocol will include a crypto challenge (say bcrypt? like bitcoin)
-    as a proof of work guard against abuse.
-    The first requested peer will challenge the new-comer.
+      The protocol will include a crypto challenge (say bcrypt? like bitcoin)
+       as a proof of work guard against abuse.
+      The first requested peer will challenge the new-comer.
     Code Sketch:
       userID = getRandomIDFromNetwork(myIP)
-    '''
+  '''
+  def join(self, knownNodes):
     # TODO(cskau): this is just example code for now.
     # We need to modify underlying network protocol for the above.
-    # DONE?
-    self.node = EntangledNode(udpPort = self.udpPort)
+    print 'joining now'
+    self.node = EntangledNode(udpPort=self.udpPort)
+    self.node.protocol = TintangledProtocol(self.node)
     self.node.joinNetwork(knownNodes)
     twisted.internet.reactor.run()
     # TODO(cskau): stub~~
@@ -68,136 +54,95 @@ class Node:
     else:
       self.userID = 'our previously issued ID'
 
+  ''' Ask network to generate a pseudo random ID for us, a la FreeNet
+  '''
   def _getRandomIDFromNetwork(self):
-    """Ask network to generate a pseudo random ID for us, a la FreeNet"""
     # TODO(cskau): stub
     return 0
 
-  def share(self, resourceID, friendsID):
-    """Share some stored resource with one or more users.
+  ''' Share some stored resource with one or more users.
     Allow other user(s) to access store resource by issuing sharing key
-    unique to the user-resource pair.
+     unique to the user-resource pair.
     Code Sketch:
       sharingKey[resourceID][otherUserID] = encrypt(
-      publicKeys[otherUserID],
-      resourceKeys[resourceID])
+          publicKeys[otherUserID],
+          resourceKeys[resourceID])
       store(
-        "SharingKey(resourceID, otherUserID)",
-        sharingKeys[resourceID][otherUserID])
-    """
-    # NOTE(purbak): using % is old-style python, use .format instead?
-    # CITATION-NEEDED(cskau): the docs still state this being the way to do it.
-    #  src: http://docs.python.org/library/stdtypes.html#string-formatting
+          "SharingKey(resourceID, otherUserID)",
+          sharingKeys[resourceID][otherUserID])
+  '''
+  def share(self, resourceID, friendsID):
     sharingKeyID = ('%s:share:%s' % (resourceID, friendsID))
     sharingKey = self._encryptForUser(self.postKeys[resourceID], friendsID)
     self.node.publishData(sharingKeyID, sharingKey)
 
+  ''' Encrypt some content assymetrically with user's public key
+  '''
   def _encryptForUser(self, content, userID):
-    """Encrypt some content asymetrically with user's public key."""
     userKey = self._getUserPublicKey(userID)
     return self._encryptKey(content, userKey)
 
   def _getUserPublicKey(self, userID):
-    """Returns the public key corresponding to the specified userID, if any."""
-    # NOTE(purbak): Why the concatenation?
-    # (cskau): ..what? this is just a semi-consistent scheme for constructing 
-    #  data labels
     publicKeyID = ('%s:publickey' % (userID))
     return self.node.iterativeFindValue(publicKeyID)
 
-  def _encryptKey(self, content, publicKey):
-    """Encrypts content (sharing key) using the specified public key."""
-    # TODO(cskau): I'm fairly certain we need to specify key here.
-    # The idea is to encrypt a peer specific sharing key under another peers 
-    #  public key, so that only he can read it.
-    return publicKey.encrypt(content, '') # '' -> K not needed when using RSA.
+  def _encryptKey(self, content, userKey):
+    # TODO(cskau): stub
+    return content
 
-  def _decryptKey(self, content):
-    """Decrypts content (sharing key) using node's own private key."""
-    return self.RSAkey.decrypt(content)
-
-  def unshare(self, resourceID, friendsID):
-    """ Unshare previously shared resource with one of more users.
+  ''' Unshare previously shared resource with one of more users.
     Ask network to delete specific, existing sharing keys.
     Note:
-    This can never be safer than the network allows it.
-    Malicious peer might simply keep the sharing keys despite all.
+      This can never be safer than the network allows it.
+      Malicious peer might simply keep the sharing keys despite all.
     Code Sketch:
       weakDelete(sharingKeys[resourceID][otherUserID])
-    """
+  '''
+  def unshare(self, resourceID, friendsID):
     shareKeyID = ('%s:share:%s' % (resourceID, friendsID))
     self.node.removeDate(shareKeyID)
 
-  def post(self, content):
-    """ Post some resource to the network.
+  ''' Post some resource to the network.
     Ask the network to store some (encrypted) resource.
     Note:
-    This should be encrypted with a symmetric key which will be private
-    until shared through the above share() method.
-    """
+      This should be encrypted with a symmetric key which will be private
+       until shared through the above share() method.
+    Code Sketch:
+      ...
+  '''
+  def post(self, content):
     newSequenceNumber = self._getSequenceNumber()
-    # NOTE(purbak): Where do you get key used in _encryptPost(key, content)
-    # from? Idea: use the _generateSymmetricKey(length) method further down in
-    # the code.
-    # (cskau): Like this?
-    postKey = _generateSymmetricKey(SYMMETRIC_KEY_LENGTH)
-    encryptedContent = self._encryptPost(postKey, content)
+    encryptedContent = self._encryptPost(key, content)
     # We need to store post keys used so we can issue sharing keys later
-    self.postKeys[newSequenceNumber] = postKey
-    # TODO(cskau): whenever we update this, we should store it securely in net
+    self.postKeys[newSequenceNumber] = key
     postID = ('%s:post:%s' % (self.userID, newSequenceNumber))
     self.node.publishData(postID, encryptedContent)
     # update our latest sequence number
     self.node.publishData('%s:latest', newSequenceNumber)
 
+  ''' Return next, unused sequence number unique to this user
+  '''
   def _getSequenceNumber(self):
-    """Return next, unused sequence number unique to this user."""
-    # TODO(cskau): we probably need to ask the network to avoid sync errors.
-    #  Case: a user might publish from multiple clients at a time.
+    # TODO(cskau): we probasbly need to ask the network to avoid sync errors
+    #  a user might publish from multiple clients at a time
     self.sequenceNumber += 1
     return self.sequenceNumber
 
+  ''' Encrypt a post with a symmetric key
+  '''
   def _encryptPost(self, key, post):
-    """Encrypt a post with a symmetric key.
+    # TODO(cskau): This is a stub
+    return post
 
-    @param key: must be 16, 24, or 32 bytes long.
-    @type key: str
-
-    """
-    if not len(key) in [16, 24, 32]:
-      raise 'aah ma gaawd!'
-    # NOTE(purbak): what to do about the nounce bit of the message.
-    # Idea: randomly generate a nounce and send along with the private key.
-    # nounce = 'abcdefghijklmnop' # TODO(purbak): Something else.
-    # TODO(cskau): Enlighten me; what are the implications of having an 
-    #  networl wide shared nounce?
-    AESkey = AES.new(key, AES.MODE_CBC, NOUNCE)
-    return AESkey.encrypt(post)
-
-  def _decryptPost(self, key, post):
-    """Decrypt a post with a symmetric key.
-
-    @param key: must be 16, 24, or 32 bytes long.
-    @type key: str
-
-    """
-    if not len(key) in [16, 24, 32]:
-      raise 'aah ma gaawd!'
-    # NOTE(purbak): what to do about the nounce bit of the message.
-    #nounce = 'abcdefghijklmnop' # TODO(purbak): Something else.
-    # TODO(cskau): see above
-    AESkey = AES.new(key, AES.MODE_CBC, NOUNCE)
-    return AESkey.decrypt(post)
-
-  def getUpdates(self, friendsID, lastKnownSequenceNumber):
-    """ Check for and fetch new updates on user(s)
+  ''' Check for and fetch new updates on user(s)
     Ask for latest known post from a given user and fetch delta since last
      fetched update.
     Code Sketch:
       latestSequenceNumber = get("latest(otherUserID)")
       latestPostID = hash(otherUserID + latestSequenceNumber)
       latestPost = get(latestPostID)
-    """
+  '''
+  def getUpdates(self, friendsID, lastKnownSequenceNumber):
     latestSequenceNumber = self.node.iterativeFindValue(
         ('%s:latest' % (friendsID)))
     delta = {}
@@ -206,24 +151,43 @@ class Node:
       delta[postID] = self.node.iterativeFindValue(postID)
     return delta
 
-  # -*- Other Encryption Methods -*-
+  # -*- Encryption Methods -*-
+
+  # * Asymmetric(RSA) *
 
   def _signMessage(message):
-    """Signs the specified message using the node's private key."""
-    hashValue = SHA.new(message).digest()
-    # TODO(cskau): fetch private key:
-    signingKey = ''
-    return self.RSAkey.sign(hashValue, signingKey)
+    '''Signs the specified message using the node's private key.'''
+    hash = SHA.new(message).digest()
+    return self.RSAkey.sign(hash, random)
 
   def _verifyMessage(message, signature):
-    """Verify a message based on the specified signature."""
-    hashValue = SHA.new(message).digest()
-    return RSAkey.verify(hashValue, signature)
+    '''Verify a message based on the specified signature.'''
+    hash = SHA.new(message).digest()
+    return RSAkey.verify(hash, signature)
 
-  def _generateSymmetricKey(keyLength):
-    """Generates a key for symmetric encryption with a byte length of "length"."""
-    return Crypto.Random.get_random_bytes(keyLength)
+  def _encryptMessageRSA(message):
+    '''Encrypts the specified message using the node's private key.'''
+    return self.RSAkey.encrypt(message, self.random.getrandbits(256))
 
+  def _decryptMessageRSA(message):
+    '''Decrypts the specified message using the node's private key.'''
+    return self.RSAkey.decrypt(message)
+
+  # * Symmetric(AES) *
+
+  def _encryptMessageAES(message, key, nounce):
+    '''Encrypts the specified message using AES.'''
+    AESobj = AES.new(key, AES.MODE_CBC, nounce)
+    return AESobj.encrypt(message)
+
+  def _encryptMessageAES(message, key, nounce):
+    '''Decrypts the specified message using the given key.'''
+    AESobj = AES.new(key, AES.MODE_CBC, nounce)
+    return AESobj.decrypt(message)
+
+  def _generateSymmetricKey(length):
+    '''Generates a key for symmetric encryption with a byte length of "length".'''
+    return "".join(chr(random.randrange(0, 256)) for i in xrange(length))
 
 if __name__ == '__main__':
   import sys
---
+++ tintangled_protocol.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: UTF-8
+from tintangled.kademlia.protocol import KademliaProtocol
+
+
+class TintangledProtocol(KademliaProtocol):
+	""" Handles and parses incoming RPC messages (and responses)
+
+        @note: This is automatically called by Twisted when the protocol
+			   receives a UDP datagram
+    """
+	def datagramReceived(self, datagram, address):
+		if datagram[0] == '\x00' and datagram[25] == '\x00':
+			totalPackets = (ord(datagram[1]) << 8) | ord(datagram[2])
+			msgID = datagram[5:25]
+			seqNumber = (ord(datagram[3]) << 8) | ord(datagram[4])
+			if msgID not in self._partialMessages:
+			    self._partialMessages[msgID] = {}
+			self._partialMessages[msgID][seqNumber] = datagram[26:]
+			if len(self._partialMessages[msgID]) == totalPackets:
+			    keys = self._partialMessages[msgID].keys()
+			    keys.sort()
+			    data = ''
+			    for key in keys:
+			        data += self._partialMessages[msgID][key]
+			        datagram = data
+			    del self._partialMessages[msgID]
+			else:
+				return
+		try:
+			msgPrimitive = self._encoder.decode(datagram)
+		except encoding.DecodeError:
+			# We received some rubbish here
+			return
+        
+		message = self._translator.fromPrimitive(msgPrimitive)
+		remoteContact = Contact(message.nodeID, address[0], address[1], self)
+        
+        #As written in s/kademlia the message is signed and actively valid, if the sender address is valid and comes from a RPC response 
+        #Actively valid sender addresses are immediately added to their corresponding bucket.
+        #Valid sender addresses are only added to a bucket if the nodeId preffix differs in an appropriate amount of bits
+		if isinstance(message, msgtypes.RequestMessage):
+			# This is an RPC method request
+			if sharesXBitPrefix(remoteContact.id, self.id, 33) == False:
+			    self._node.addContact(remoteContact)
+
+			self._handleRPC(remoteContact, message.id, message.request, message.args)
+		elif isinstance(message, msgtypes.ResponseMessage):
+			# Find the message that triggered this response
+			# Refresh the remote node's details in the local node's k-buckets
+			self._node.addContact(remoteContact)
+        
+			if self._sentMessages.has_key(message.id):
+			    # Cancel timeout timer for this RPC
+			    df, timeoutCall = self._sentMessages[message.id][1:3]
+			    timeoutCall.cancel()
+			    del self._sentMessages[message.id]
+
+			    if hasattr(df, '_rpcRawResponse'):
+			        # The RPC requested that the raw response message and originating address be returned; do not interpret it
+			        df.callback((message, address))
+			    elif isinstance(message, msgtypes.ErrorMessage):
+			        # The RPC request raised a remote exception; raise it locally
+			        if message.exceptionType.startswith('exceptions.'):
+						exceptionClassName = message.exceptionType[11:]
+			        else:
+						localModuleHierarchy = self.__module__.split('.')
+						remoteHierarchy = message.exceptionType.split('.')
+						#strip the remote hierarchy
+						while remoteHierarchy[0] == localModuleHierarchy[0]:
+							remoteHierarchy.pop(0)
+							localModuleHierarchy.pop(0)
+						exceptionClassName = '.'.join(remoteHierarchy)
+			        remoteException = None
+			        try:
+						exec 'remoteException = %s("%s")' % (exceptionClassName, message.response)
+			        except Exception:
+					# We could not recreate the exception; create a generic one
+						remoteException = Exception(message.response)
+			        df.errback(remoteException)
+			    else:
+			        # We got a result from the RPC
+			        df.callback(message.response)
+			else:
+			    # If the original message isn't found, it must have timed out
+			    #TODO: we should probably do something with this...
+			    pass
