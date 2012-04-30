d0e7ca982e41fceb14296d221122ec5a45fb1079
Allan <allan.stisen@gmail.com>
2012-04-30 10:38:02 +0200

    Made a designated tintangleddefaultformat in order to chekc that nodeids are correct and their corresponding X

--- msgformat.py
+++ msgformat.py
@@ -6,25 +6,39 @@ from entangled.kademlia.msgformat import MessageTranslator
 class TintangledDefaultFormat(MessageTranslator):
     """ The default on-the-wire message format for this library """
     typeRequest, typeResponse, typeError = range(3)
-    headerType, headerMsgID, headerNodeID, headerCryptoChallengeX, header_public_key_n, headerPayload, headerArgs = range(7)
+    headerType, headerMsgID, headerNodeID, headerCryptoChallengeX, 
+    header_public_key_n, headerPayload, headerArgs = range(7)
     
     def fromPrimitive(self, msgPrimitive):
         msgType = msgPrimitive[self.headerType]
         if msgType == self.typeRequest:
-            msg = msgtypes.RequestMessage(msgPrimitive[self.headerNodeID], msgPrimitive[self.header_public_key_n], 
-                msgPrimitive[self.headerCryptoChallengeX], msgPrimitive[self.headerPayload], msgPrimitive[self.headerArgs], msgPrimitive[self.headerMsgID])
+            msg = msgtypes.RequestMessage(msgPrimitive[self.headerNodeID],
+                msgPrimitive[self.header_public_key_n],
+                msgPrimitive[self.headerCryptoChallengeX], 
+                msgPrimitive[self.headerPayload], 
+                msgPrimitive[self.headerArgs], 
+                msgPrimitive[self.headerMsgID])
         elif msgType == self.typeResponse:
-            msg = msgtypes.ResponseMessage(msgPrimitive[self.headerMsgID], msgPrimitive[self.headerNodeID], msgPrimitive[self.header_public_key_n], 
-                msgPrimitive[self.headerCryptoChallengeX], msgPrimitive[self.headerPayload])
+            msg = msgtypes.ResponseMessage(msgPrimitive[self.headerMsgID], 
+                msgPrimitive[self.headerNodeID], 
+                msgPrimitive[self.header_public_key_n], 
+                msgPrimitive[self.headerCryptoChallengeX], 
+                msgPrimitive[self.headerPayload])
         elif msgType == self.typeError:
-            msg = msgtypes.ErrorMessage(msgPrimitive[self.headerMsgID], msgPrimitive[self.headerNodeID], msgPrimitive[self.header_public_key_n], 
-                msgPrimitive[self.headerCryptoChallengeX], msgPrimitive[self.headerPayload], msgPrimitive[self.headerArgs])
+            msg = msgtypes.ErrorMessage(msgPrimitive[self.headerMsgID], 
+                msgPrimitive[self.headerNodeID], 
+                msgPrimitive[self.header_public_key_n], 
+                msgPrimitive[self.headerCryptoChallengeX], 
+                msgPrimitive[self.headerPayload], 
+                msgPrimitive[self.headerArgs])
         else:
             # Unknown message, no payload
-            msg = msgtypes.Message(msgPrimitive[self.headerMsgID], msgPrimitive[self.headerNodeID],msgPrimitive[self.header_public_key_n], 
+            msg = msgtypes.Message(msgPrimitive[self.headerMsgID], 
+                msgPrimitive[self.headerNodeID],
+                msgPrimitive[self.header_public_key_n], 
                 msgPrimitive[self.headerCryptoChallengeX])
         return msg
-    
+
     def toPrimitive(self, message):    
         msg = {self.headerMsgID:  message.id,
                self.headerNodeID: message.nodeID,
